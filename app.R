34129931740d941859e3cc1b1e2cda130ea4e634
Shumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-09-19 13:08:54 -0400

    Adds link to CSV file containing data

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 library(shiny)
 library(shinydashboard)
 
+recommendation <- read.csv('https://raw.githubusercontent.com/amrrs/sample_revenue_dashboard_shiny/master/recommendation.csv',stringsAsFactors = F,header=T)
+
 ui <- dashboardPage(
     dashboardHeader(),
     dashboardSidebar(),
